425cbb2c535a2269fd4f0cf5e3382a9d7066c8f6
Victor <publicdomainvic@gmail.com>
2021-10-12 19:26:18 -0400

    Add single instance code

--- HotkeyDraw/Program.cs
+++ HotkeyDraw/Program.cs
@@ -1,24 +1,34 @@
 ﻿
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace HotkeyDraw
 {
-	/// <summary>
-	/// Class with program entry point.
-	/// </summary>
-	internal sealed class Program
-	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
-		[STAThread]
-		private static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
-		}
-		
-	}
+    /// <summary>
+    /// Class with program entry point.
+    /// </summary>
+    internal sealed class Program
+    {
+        static Mutex mutex = new Mutex(false, "HotkeyDraw");
+
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (!mutex.WaitOne(TimeSpan.Zero, false))
+            {
+                MessageBox.Show("HotkeyDraw already started!", "Single instance", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+    }
 }
